423003aa032f3b965fa5959c2db1af677ef21460
Carlos Hilner <hilner@alu.ufc.br>
2019-02-08 10:01:39 -0300

    Update SpendingAccountController.java

--- src/main/java/com/bankapp/controller/SpendingAccountController.java
+++ src/main/java/com/bankapp/controller/SpendingAccountController.java
@@ -15,7 +15,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "https://progressivebankapp.herokuapp.com")
+//@CrossOrigin(origins = "https://progressivebankapp.herokuapp.com")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/account/spending")
 public class SpendingAccountController {
