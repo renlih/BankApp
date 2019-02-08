80bbcd38ee10a0db4d9872e9845846d8912fda9c
Carlos Hilner <hilner@alu.ufc.br>
2019-02-08 10:01:03 -0300

    Update ClientController.java

--- src/main/java/com/bankapp/controller/ClientController.java
+++ src/main/java/com/bankapp/controller/ClientController.java
@@ -14,7 +14,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "https://progressivebankapp.herokuapp.com")
+//@CrossOrigin(origins = "https://progressivebankapp.herokuapp.com")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/client")
 public class ClientController {
